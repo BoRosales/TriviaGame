dfd3fd0ac4c7e7e8fe6828bb4e057d67997e19d0
huesos <bonirosales@gmail.com>
2018-09-04 19:57:13 -0500

    stuff

---
+++ assets/javascript/app.js
@@ -0,0 +1,82 @@
+//Global variables
+let correct = 0;
+let incorrect = 0;
+let unanswered = 0;
+let myTimer = 15;
+let myTimeCounter;
+let timeOut;
+let counter = 1;
+let questions = [
+    {
+    q1: 1,
+    ask: "What type of bat lives in Austin?",
+    choices1: "Mexican free-tailed bat",
+    choices2: "Batman",
+    choices3: "Vampire bat",
+    choices4: "Megabats",
+    correctAns: "Mexican free-tailed bat"
+},
+{
+    q2: 2,
+    ask: "What does a Mexican free-tailed bat eat?",
+    choices1: "Blood",
+    choices2: "Insects",
+    choices3: "Torchys",
+    choices4: "Fruit",
+    correctAns: "Insects",
+},
+{
+    q3: 3,
+    ask: "When do the bats call congress home?",
+    choices1: "During UT Football season", 
+    choices2: "From March to November",
+    choices3: "December to Janruary",
+    choices4: "All year round",
+    correctAns: "from March to November",
+}
+
+]
+
+//The game starts here //
+function startGame() {
+    $(".guess-pack").removeClass('hide');
+    displayAsk();
+};
+//The timer starts counting down, asks the question, and shows the choices to pick from
+function displayAsk() {
+    $(".ask-question").html('<h3>Question ' + parseInt(counter) + '/' + parseInt(questions.length) + '</h3>');
+    $(".timer").html('<h2>Timer: ' + myTimer + 's</h2>')
+    myTimeCounter = setInterval(timer, 1000);
+    $(".asked-question").html('<h3>' + questions[counter].ask + '</h3>');
+    displayChoices();
+    timeOut = setTimeout(timeDone, 1000 * 15);
+};
+
+function timer() {
+    myTimer--;
+    $(".timer").html('<h3>Timer: ' + myTimer + 's</h3>');
+};
+
+function displayChoices() {
+    $(".choices1").html(questions[counter].choices1);
+    $(".choices2").html(questions[counter].choices2);
+    $(".choices3").html(questions[counter].choices3);
+    $(".choices4").html(questions[counter].choices4);
+};
+
+function timeDone() {
+    $(".guess-pack").addClass('hide');
+    $(".outcome").removeClass('hide');
+    $(".asked-question").addClass('hide');
+    clearInterval(myTimeCounter);
+    clearInterval(timeOut);
+    unanswered++;
+    $(".outcome").html("<h1>You're out of time!</h1>");
+    settimeDone(nextQuestion, 3000);
+};
+
+function nextQuestion() {
+    $()
+};
+
+//});
